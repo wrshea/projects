aa3490d9fb0bd8512e5f817bf991e2f1d3f96f59
William Shea <william.r.shea@gmail.com>
2019-03-11 00:22:10 +0000

    bash script to unzip files

--- unpack.sh
+++ unpack.sh
@@ -2,7 +2,7 @@
 
 #this script unpacks .tar.gz files to week7 directory
 cd work/week7
-for value in ~/work/week6/cwl-data/data/structured/structured-2018*.tar.gz
+for value in ~/work/week7/data/structured/structured-2018*.tar.gz
 do
    tar zxvf $value
 done
